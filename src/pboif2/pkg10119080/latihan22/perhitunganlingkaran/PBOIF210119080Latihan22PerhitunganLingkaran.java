a4a23678ffa3ef7f2f9bc6c0293218b1dee125ce
PratamaVin7 <kevinpratamaoke@gmail.com>
2020-11-01 11:53:15 +0700

    First commit

---
+++ src/pboif2/pkg10119080/latihan22/perhitunganlingkaran/PBOIF210119080Latihan22PerhitunganLingkaran.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119080.latihan22.perhitunganlingkaran;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author
+ * NAMA     : Kevin Pratama Sinaga
+ * KELAS    : PBOIF2
+ * NIM      : 10119080
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * Perhitungan Lingkaran
+ */
+public class PBOIF210119080Latihan22PerhitunganLingkaran {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double diameter, jarijari, luas, keliling;
+        final double PHI = 3.14;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("======Perhitungan Lingkaran======");
+        System.out.print("Masukkan nilai diameter lingkaran : ");
+        while(true) {
+            try {
+                diameter = Double.parseDouble(scanner.next());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Nilai Diameter Tidak Sesuai\n");
+                System.out.print("Masukkan nilai diameter lingkaran : ");
+                continue;
+            }
+        }
+        jarijari = diameter /2 ;
+        luas = PHI * jarijari * jarijari;
+        keliling = PHI * diameter;
+        System.out.println();
+        System.out.println("======Hasil Perhitungan Lingkaran=====");
+        System.out.printf("Jari-jari Lingkaran\t\t= %.2f%n", jarijari);
+        System.out.printf("Luas Lingkaran\t\t\t= %.2f%n", luas);
+        System.out.printf("Keliling Lingkaran\t\t= %.2f%n", keliling);
+    }
+    
+}
